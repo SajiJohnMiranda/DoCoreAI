e97331ed0df7eb6da5e28136470968964b67fa10
Saji John Miranda <sajijohnmiranda@gmail.com>
2025-03-29 08:12:10 +0530

    added contributing-testers.py

--- tests/Quick Test/test.py
+++ tests/Quick Test/test.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from docore_ai import intelligence_profiler # Sample DoCoreAI program to demonstrate
+from docore_ai.model import intelligence_profiler,normal_prompt  # Test DoCoreAI program 
 
 dotenv_loaded=load_dotenv()
 print("Welcome to DoCoreAI - Intelligence Profiler")
@@ -11,11 +11,26 @@ print("DEBUG: ENV VARIABLES:")
 print("MODEL_PROVIDER:", os.getenv("MODEL_PROVIDER"))# DEFAULT_MODEL='' - use 'openai','groq' etc in .env file
 print("MODEL_NAME:", os.getenv("MODEL_NAME"))# MODEL='' - use   'gpt-3.5-turbo', 'gemma2-9b-it etc' in the .env file
 
+# TESTERS can change it here
+PROMPT ='What is one good way to start python coding for a experienced programmer'
+ROLE='AI Developer'
+
 def main():
+    print('Normal Response:')
+    print('---------------')
     print(
-        intelligence_profiler("What is one good way to start python coding for a experienced programmer","AI Developer",
+        normal_prompt(PROMPT,ROLE,
                               os.getenv("MODEL_PROVIDER"),
                               os.getenv("MODEL_NAME")))
-
+    print('*---------------')
+    print('---------------*')
+    print('DoCoreAI Response:')
+    print('-----------------')
+    print(
+        intelligence_profiler(PROMPT,ROLE,
+                              os.getenv("MODEL_PROVIDER"),
+                              os.getenv("MODEL_NAME")))
+    print('*---------------')
+    print('---------------*')
 if __name__=="__main__":
     main()
